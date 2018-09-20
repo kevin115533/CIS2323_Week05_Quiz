00554ef994fd95c06af112b1c8197e24151f796d
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-09-19 20:10:27 -0500

    print list and mark done methods working

--- ToDoList.java
+++ ToDoList.java
@@ -9,23 +9,16 @@ public class ToDoList{
 		String todo = "";
 		
 		ToDo[] userList = new ToDo[MAXTODO];
-		
-		while (keepGoing == true){
 		Scanner userInput = new Scanner(System.in);
+		while (keepGoing == true){
 		
-		System.out.println("==================================================");
-		System.out.println("What would you like to do? Pick an option number ");
-		System.out.println("1 - Show ToDo List");
-		System.out.println("2 - Add to ToDo List");
-		System.out.println("3 - Mark ToDo item as done ");
-		System.out.println("4 - Print count of ToDos ");
-		System.out.println("==================================================");
+		userOpt= printMenu();
 		
-		userOpt = userInput.nextInt();
-		userInput.nextLine();
 		
 		if(userOpt == 1){
-			//userList.getToDo();
+			for (int i = 0;i<ToDo.count; i++){
+			System.out.println((i+1) + " ToDo: " + userList[i].getToDo());
+			}
 		}
 		else if(userOpt == 2){
 			System.out.println("What is your task? ");
@@ -36,7 +29,10 @@ public class ToDoList{
 			
 		}
 		else if(userOpt == 3){
-			
+			System.out.println("What do you want to mark done? ");
+			userOpt = userInput.nextInt();
+			userInput.nextLine();
+			userList[(userOpt-1)].markDone();
 		}
 		else if(userOpt == 4){
 			System.out.println("You have " + ToDo.count + " ToDo items.");
@@ -44,4 +40,20 @@ public class ToDoList{
 		}
 	}
 	
+	private static int printMenu(){
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("==================================================");
+		System.out.println("What would you like to do? Pick an option number ");
+		System.out.println("1 - Show ToDo List");
+		System.out.println("2 - Add to ToDo List");
+		System.out.println("3 - Mark ToDo item as done ");
+		System.out.println("4 - Print count of ToDos ");
+		System.out.println("==================================================");
+		
+		int userOpt = userInput.nextInt();
+		userInput.nextLine();
+		
+		return userOpt;
+		
+	}
 }
